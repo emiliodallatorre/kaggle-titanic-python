8cae5eb4d2c8c393ac868e09d37bcb53820567fd
Emilio Dalla Torre <emilio.dalla.torre@pwc.com>
2025-01-31 11:27:14 +0100

    Updated submit script

--- submit.sh
+++ submit.sh
@@ -1,1 +1,6 @@
-kaggle competitions submit -c titanic -f output.csv -m $1
+echo "Generating output..."
+python3 main.py || exit 1
+
+MESSAGE=${1:-"$(date '+%Y-%m-%d %H:%M:%S')"}
+echo "Submitting output..."
+kaggle competitions submit -c titanic -f output.csv -m "$MESSAGE"
