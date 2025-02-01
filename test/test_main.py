11757e9e2c77ab3430be820ef74018a70c1b30fb
Emilio Dalla Torre <info@emiliodallatorre.it>
2025-02-01 19:03:07 +0100

    Fixes to the first test

--- test/test_main.py
+++ test/test_main.py
@@ -4,13 +4,15 @@ import main
 
 
 class TestMain(TestCase):
+    OUTPUT_FILE: str = "output.csv"
+
     def test_main(self):
         os.chdir("..")
 
-        if os.path.exists("output.csv"):
-            os.remove("output.csv")
+        if os.path.exists(self.OUTPUT_FILE):
+            os.remove(self.OUTPUT_FILE)
 
         main.main()
 
-        if not os.path.exists("output.csv"):
+        if not os.path.exists(self.OUTPUT_FILE):
             self.fail()
